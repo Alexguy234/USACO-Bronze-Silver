9da55a37e8fc00f4275ab2c6d7347300a92090c8
Alex Sui <146045160+Alexguy234@users.noreply.github.com>
2026-08-28 10:16:15 -0800

    added milk pails

--- Silver/2016_feb_milk_pails.cpp
+++ Silver/2016_feb_milk_pails.cpp
@@ -16,8 +16,11 @@
 using namespace std;
 int ans = INT_MAX;
 //optimize using memoization
+vector<vector<vector<bool>>> dp(120, vector<vector<bool>>(105, vector<bool>(120, false)));
 void dfs(int x, int y, int k, int m, int cur_x, int cur_y, int step){
-    ans = min(ans, abs(m - (x + y)));
+    if (dp[cur_x][cur_y][step]) return;
+    ans = min(ans, abs(m - (cur_x + cur_y)));
+    dp[cur_x][cur_y][step] = true;
     if (step == k){
         return;
     }
@@ -33,15 +36,17 @@ void dfs(int x, int y, int k, int m, int cur_x, int cur_y, int step){
     }
 
     if (cur_y + cur_x > y){
-        dfs(x, y, k, m, cur_x - (y - cur_y), 0, step + 1);
+        dfs(x, y, k, m, cur_x - (y - cur_y), y, step + 1);
     }
     else{
         dfs(x, y, k, m, 0, cur_y + cur_x, step + 1);
     }
 }
 int main(){
+    ifstream fin ("pails.in");
+    ofstream fout ("pails.out");
     int x, y, k, m;
-    cin >> x >> y >> k >> m;
+    fin >> x >> y >> k >> m;
     dfs(x, y, k, m, 0, 0, 0);
-    cout << ans << endl;
+    fout << ans << endl;
 }
